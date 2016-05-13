aed794225c11d0d5942ee0d6137e0cad5d03b48f
TheDeveloperXYZ <contact@thedeveloper.xyz>
2016-05-13 14:56:17 +0300

    Update

--- source/index.js
+++ source/index.js
@@ -1,5 +1,47 @@
-export default class {
-  constructor () {
-    console.log('Hello');
-  }
+const TYPE_ARRAY = '[object Array]';
+const TYPE_STRING = '[object String]';
+const TYPE_OBJECT = '[object Object]';
+const TYPE_NUMBER = '[object Number]';
+
+function getType (type) {
+  return Object.prototype.toString.call(type);
+}
+
+function isNumber (value) {
+  return !!(getType(value) === TYPE_NUMBER);
+}
+
+function isString (value) {
+  return !!(getType(value) === TYPE_STRING);
+}
+
+function isObject (value) {
+  return !!(getType(value) === TYPE_OBJECT);
+}
+
+function isArray (value) {
+  return !!(getType(value) === TYPE_ARRAY);
+}
+
+export default function (options) {
+  return function () {
+    const rootName = options.name || null;
+    var classNames = [];
+
+    for (let arg of arguments) {
+      if (isString(arg) || isNumber(arg)) {
+        classNames.push(arg);
+      } else if (isObject(arg)) {
+        for (var key in arg) {
+          (arg.hasOwnProperty(key) && arg[key]) && classNames.push(key);
+        }
+      } else if (isArray(arg)) {
+        classNames.push(...arg);
+      }
+    }
+
+    classNames = classNames.map((cn) => `${rootName}__${cn}`);
+
+    return classNames.join(' ');
+  };
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,28 @@ import expect from 'unexpected';
 import TTClassNames from '../source';
 
 describe('TTClassNames', () => {
-  const classNames = new TTClassNames();
+  const classNames = new TTClassNames({
+    name: 'tt'
+  });
 
   it('should be defined', () => {
     expect(classNames, 'to be defined');
+  });
+
+  describe('return values', () => {
+    it('should return correct classname', () => {
+      expect(classNames('test'), 'to be', 'tt__test');
+
+      expect(classNames({
+        'cool': true,
+        'weird': false
+      }), 'to be', 'tt__cool');
+
+      expect(classNames(['test', 123]), 'to be', 'tt__test tt__123');
+
+      expect(classNames('test', 123, ['what'], {
+        'trip-trax': true
+      }), 'to be', 'tt__test tt__123 tt__what tt__trip-trax');
+    });
   });
 });
