a61bed8ab54066fd05ce472337fe94e16461364d
TheDeveloperXYZ <contact@thedeveloper.xyz>
2016-05-13 14:56:52 +0300

    Dist update

--- dist/tt-classnames.js
+++ dist/tt-classnames.js
@@ -60,15 +60,80 @@ return /******/ (function(modules) { // webpackBootstrap
 	  value: true
 	});
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	exports.default = function (options) {
+	  return function () {
+	    var rootName = options.name || null;
+	    var classNames = [];
 
-	var _class = function _class() {
-	  _classCallCheck(this, _class);
+	    var _iteratorNormalCompletion = true;
+	    var _didIteratorError = false;
+	    var _iteratorError = undefined;
 
-	  console.log('Hello');
+	    try {
+	      for (var _iterator = arguments[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+	        var arg = _step.value;
+
+	        if (isString(arg) || isNumber(arg)) {
+	          classNames.push(arg);
+	        } else if (isObject(arg)) {
+	          for (var key in arg) {
+	            arg.hasOwnProperty(key) && arg[key] && classNames.push(key);
+	          }
+	        } else if (isArray(arg)) {
+	          var _classNames;
+
+	          (_classNames = classNames).push.apply(_classNames, _toConsumableArray(arg));
+	        }
+	      }
+	    } catch (err) {
+	      _didIteratorError = true;
+	      _iteratorError = err;
+	    } finally {
+	      try {
+	        if (!_iteratorNormalCompletion && _iterator.return) {
+	          _iterator.return();
+	        }
+	      } finally {
+	        if (_didIteratorError) {
+	          throw _iteratorError;
+	        }
+	      }
+	    }
+
+	    classNames = classNames.map(function (cn) {
+	      return rootName + '__' + cn;
+	    });
+
+	    return classNames.join(' ');
+	  };
 	};
 
-	exports.default = _class;
+	function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+	var TYPE_ARRAY = '[object Array]';
+	var TYPE_STRING = '[object String]';
+	var TYPE_OBJECT = '[object Object]';
+	var TYPE_NUMBER = '[object Number]';
+
+	function getType(type) {
+	  return Object.prototype.toString.call(type);
+	}
+
+	function isNumber(value) {
+	  return !!(getType(value) === TYPE_NUMBER);
+	}
+
+	function isString(value) {
+	  return !!(getType(value) === TYPE_STRING);
+	}
+
+	function isObject(value) {
+	  return !!(getType(value) === TYPE_OBJECT);
+	}
+
+	function isArray(value) {
+	  return !!(getType(value) === TYPE_ARRAY);
+	}
 
 /***/ }
 /******/ ])
